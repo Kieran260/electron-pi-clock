c564e1ad466e7a7b67892bc9bb2fa419063eb8b9
Kieran Hardwick <kieranhardwick260@gmail.com>
2023-03-28 14:43:52 +0100

    Changed screen size requirements

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ dotenv.config();
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 480,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
